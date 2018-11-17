dbb92868b6708aff3aeb78617563d2e834fd983d
Andre Lima <hello@andrelima.space>
2018-11-17 19:47:15 -0200

    Updates

---
+++ precache-manifest.2e1cc47f1f0181482dba0d4f17c2a55e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e1c0249b9c928f2dbc4",
+    "url": "/reminderpro/static/js/runtime~main.2e1c0249.js"
+  },
+  {
+    "revision": "7636ab905ee5c4255a22",
+    "url": "/reminderpro/static/js/main.7636ab90.chunk.js"
+  },
+  {
+    "revision": "4641ba7f542f7345f19b",
+    "url": "/reminderpro/static/js/1.4641ba7f.chunk.js"
+  },
+  {
+    "revision": "7636ab905ee5c4255a22",
+    "url": "/reminderpro/static/css/main.5e68d4e9.chunk.css"
+  },
+  {
+    "revision": "7100d845c3bab9cd758ff11e0f1b5ec8",
+    "url": "/reminderpro/index.html"
+  }
+];
